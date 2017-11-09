34dcc6264df6a205ff340c9fbf6ab26facf3a4bb
Micha Alt <mail@lean-stack.de>
2017-11-09 14:10:37 +0100

    content: added title back.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,15 +17,26 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'A Developers Notebook'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.brandName).toEqual('Developers Notebook');
+    expect(app.title).toEqual('A Developers Notebook');
+  }));
+  it(`should have a brand name 'Lean Stack'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.brandName).toEqual('Lean Stack');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.navbar-brand').textContent).toContain('Developers Notebook');
+    expect(compiled.querySelector('h1').textContent).toContain('A Developers Notebook');
+  }));
+  it('should render brand name in the navbar brand anchor', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.navbar a.navbar-brand').textContent).toContain('Lean Stack');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  brandName = 'Developers Notebook';
+  brandName = 'Lean Stack';
+  title = 'A Developers Notebook';
 }
